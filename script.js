309531482f233ce0e35f2a5cd17faccd1c5b2374
maksimsavi <savianokm@gmail.com>
2020-06-10 20:03:29 -0500

    just in case

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ const APIkey = '';
 const fetchOptions = { 'X-Api-Key': 'sVI3vB7wsPwAYW0WWSbEeut3oOfV8E2PX6Fr7EMt'};
 //catches response and passes to fetch
 function catchResponse() {
+    console.log ('listening for input.');
     $("#locationInput").submit(function( event ) {
         event.preventDefault();
+        
         let locationInput = $("input[type=text][name=location]").val()
             resultmax = $("input[type=number][name=maxlimit]").val()
             state = $("#state").val();
+            console.log('fetching data...');
             fetchData (locationInput, resultmax, state);
         });
-    console.log ('catch response ran');
+    
 }
 function fetchData (locationInput, resultmax, state) {
     
-    fetch('https://developer.nps.gov/api/v1/parks?limit=10&stateCode=', {   
-        headers: {
-          'X-Api-Key': 'sVI3vB7wsPwAYW0WWSbEeut3oOfV8E2PX6Fr7EMt'
-        }})
+    fetch(`https://developer.nps.gov/api/v1/parks?api_key=sVI3vB7wsPwAYW0WWSbEeut3oOfV8E2PX6Fr7EMt&limit=${resultmax}&stateCode=${state}`)
   .then(res => {
     if (!res.ok) {
         return res.json().then(json => {
@@ -26,18 +26,32 @@ function fetchData (locationInput, resultmax, state) {
     return res.json();
   })
   .then(json => {
-    console.log(json);
+    processData(json);
   })
   .catch(error => {
     alert(error)
   });
 }
-function processData() {
-    let arraytest = ['park 1','park 2','park 3']
-
-}
-function postData(){
-    
+function processData(json) {
+    console.log(json);
+    let dataArray = [];
+    json.data.forEach(element => 
+        dataArray.push(`
+        <li>
+        <ul>
+        <li><a href="${element.url}" target="_blank">${element.name}</a></li>
+        <li>${element.description}</li>
+        </ul>
+        </li>
+        `));
+        
+        postData(dataArray);
+    }
+function postData(dataArray){
+    dataArray.forEach(element => 
+        $( "#results>ul" ).append(element)    
+        );
+        $( "#results" ).removeClass( "hidden" )
 }
 function bootLogic() {
     catchResponse();
